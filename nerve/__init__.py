e9da83c4b63adda9b20799188b8a86b99c0e37cf
mdadda <49372461+mdadda@users.noreply.github.com>
2022-04-05 18:00:12 +0100

    Delete nerve directory

--- nerve/__init__.py
+++
@@ -1,6 +0,0 @@
-from nerve.nerve import (crm,
-                         get_clean_entities,
-                         get_entities,
-                         normalize_confusion_matrix,
-                         performance_metrics,
-                         plot_confusion_matrix)
