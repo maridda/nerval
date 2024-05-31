250dbb3d8296aca24964023b74f8858e99b7226e
maridda <49372461+maridda@users.noreply.github.com>
2024-05-31 17:59:35 +0100

    fix typo

--- nerval/__init__.py
+++ nerval/__init__.py
@@ -1,6 +1,8 @@
-from nerval.nerval import (crm,
-                           get_clean_entities,
-                           get_entities,
-                           normalize_confusion_matrix,
-                           performance_metrics,
-                           plot_confusion_matrix)
+from nerval.nerval import (
+    crm,
+    get_clean_entities,
+    get_entities,
+    normalize_confusion_matrix,
+    performance_metrics,
+    plot_confusion_matrix
+)
